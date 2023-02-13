dd6e67b45f2a3da47939baa2ee243f492729da8f
lucasBorcher <124712928+lucasBorcher@users.noreply.github.com>
2023-02-13 17:39:33 +0100

    Add files via upload

---
+++ JavaScript/Knap.js
@@ -0,0 +1,8 @@
+const buttonContainer = document.getElementById("buttoncontainer");
+buttonContainer.addEventListener("click", function () {
+  buttonContainer.style.display = "none";
+});
+const messagecontainer = document.getElementById("messagecontainer");
+buttonContainer.addEventListener("click", function () {
+    messagecontainer.style.display = "flex";
+  });
